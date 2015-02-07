a1e3d9544e4a81fac739c9b2a0845e8fbd5fe946
Michael Hopper <drmikeh@gmail.com>
2015-02-07 18:12:14 -0500

    Added Bourbon.

--- Gemfile
+++ Gemfile
@@ -3,3 +3,7 @@ source 'https://rubygems.org'
 ruby '2.1.5'
 
 gem 'sinatra'
+gem 'sass'
+gem 'bourbon'
+gem 'neat'
+gem 'bitters'
--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require 'sinatra'
+require 'sass'
+require 'bourbon'
+require 'neat'
+
+get('/styles/main.css') do
+  content_type 'text/css', charset: 'utf-8'
+  scss :'../public/styles/main'
+end
 
 get '/' do
   erb :home
